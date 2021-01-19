c803cfaba0b17c8ee8b9a858725598864362c04b
AliHaidry <alihaidry11@gmail.com>
2021-01-19 15:00:04 -0500

    fourth commit minor code changes

--- primeFactGraph.py
+++ primeFactGraph.py
@@ -1,19 +1,17 @@
-# Finding Prime numbers between given intervals
-
 
 import time
 import matplotlib.pyplot as plt
-import numpy as np
 
-LowerBound = int(input("input the lower value:   "))
-UpperBound = int(input("input the upper value:    "))
+
+LowerBound = int(input("Enter lower range value:   "))
+UpperBound = int(input("Enter upper range value:    "))
 
 start = time.time()
 
 prime = []
 timeTracker = []
 for priNum in range(LowerBound, UpperBound + 1):
-    # all prime numbers are greater than 1
+    # prime numbers greater > 1
     if priNum > 1:
         for i in range(2, priNum):
             if (priNum % i) == 0:
@@ -24,7 +22,7 @@ for priNum in range(LowerBound, UpperBound + 1):
 
 print("The generated prime numbers in given interval are:", prime)
 
-# ploting the prime values and according to their discovering time
+# plotting prime values with their discovering time
 plt.plot(timeTracker, prime, color='tab:red')
 plt.ylabel('Prime Number')
 plt.xlabel('Time Taken')
